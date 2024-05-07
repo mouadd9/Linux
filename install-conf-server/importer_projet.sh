32918af52c882066d3ddf18cbbf26cd17a786f7b
mouadispunk <bencaidmouad9@gmail.com>
2024-05-07 07:09:16 +0100

    i made it possible so you can push to remote repositories from within the container

--- install-conf-server/importer_projet.sh
+++ install-conf-server/importer_projet.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 git clone https://github.com/mouadd9/login_register.git /var/www/html
+
+cd /var/www/html
+
+git config --global user.name "mouadd9"
+git config --global user.email "bencaidmouad9@gmail.com"
+
+git remote add origin https://github.com/mouadd9/login_register.git
