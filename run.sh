1995dc1a4ecde27b5d359109cbc7842260e46931
mouadispunk <bencaidmouad9@gmail.com>
2024-05-11 07:51:20 +0100

    no cache

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ mkdir -p sql/
 mkdir -p ressources/
 chmod 755 ressources/
 chmod 755 sql/
-docker run -it -v "$(pwd)/sql:/var/lib/mysql" -v "$(pwd)/ressources:/var/www/html" \ 
+docker run -it -v "$(pwd)/sql:/var/lib/mysql" -v "$(pwd)/ressources:/var/www/html" \
     -p 80:80 -p 3306:3306 \
     --name container --cap-add=NET_ADMIN image 
 
